97a5dff17b27d51fae26db479927972baf768cb1
shubhanshu7532 <69970271+shubhanshu7532@users.noreply.github.com>
2024-12-03 23:28:15 +0530

    cron changes

--- crons.js
+++ crons.js
@@ -48,10 +48,13 @@ function initCronJobs() {
   // daily email sending cron
   // 30 * * * * *
   // 58 23 * * *
-  cron.schedule("30 0 * * *", async () => {
+  // 0 0 19 * * *
+
+  // 'vishal@gsxsolutions.com','sg@gsxtech.io'
+  cron.schedule("0 0 19 * * *", async () => {
     try{
       console.log("every 30 second cron")
-      const url = await downloadEmailReport()
+      const {url,payhub_data,merchant_data,gateway_data} = await downloadEmailReport()
 
       console.log("here is my urlllll",url)
 
@@ -64,10 +67,20 @@ function initCronJobs() {
           pass: 'payhub123$'  // Replace with your email password
         }
       });
+
+
+      const today = new Date(Date.now());
+const yesterday = new Date(today.setDate(today.getDate() - 1)); // Subtract one day
+
+const dd = String(yesterday.getDate()).padStart(2, '0');
+const mm = String(yesterday.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+const yyyy = yesterday.getFullYear();
+
+const formattedDate = `${dd}/${mm}/${yyyy}`
   
       // Define recipients
       
-      const recipients = ['tshubhanshu007@gmail.com','vishal@gsxsolutions.com','sg@gsxtech.io','tushant029@gmail.com'];  // Replace with actual email addresses
+      const recipients = ['tshubhanshu007@gmail.com','tushant029@gmail.com','vishal@gsxsolutions.com','sg@gsxtech.io'];  // Replace with actual email addresses
   
       // Create mail options for sending the email
       const mailOptions = {
@@ -77,24 +90,271 @@ function initCronJobs() {
         },
         to: recipients.join(','),  // Send to both emails
         subject: "24 Hour Report of Payhub",
-        html: `
-        <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; color: #333;">
-          <div style="max-width: 600px; margin: auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
-            <h2 style="text-align: center; color: #000; border-bottom: 2px solid #000; padding-bottom: 10px;">PayHub</h2>
-            <p style="font-size: 16px; line-height: 1.5;">Hello,</p>
-            <p style="font-size: 16px; line-height: 1.5;">Here is the 24-hour report of Payhub:</p>
-            <div style="text-align: center; margin: 20px 0;">
-              <a href="${url}" style="background-color: #007BFF; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 18px;">Download Report</a>
-            </div>
-            <p style="font-size: 16px; line-height: 1.5;">Please click the button to download your daily CSV report.</p>
-            <p style="font-size: 16px; line-height: 1.5;">Thank you,</p>
-            <p style="font-size: 16px; line-height: 1.5;">The Payhub Team</p>
-            <hr style="border: 0; height: 1px; background: #ddd; margin: 20px 0;">
-            <small style="color: #666; font-size: 12px;">If you have any questions, feel free to reach out to us at <a href="mailto:support@payhub.com" style="color: #000; text-decoration: none;">support@payhub.com</a></small>
-          </div>
-        </div>
-        `
+        html : `
+        <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; color: #333;">
+  <div style="max-width: 600px; margin: auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);">
+   <!-- Header with Date -->
+    <table width="100%" style="margin-bottom: 20px;">
+      <tr>
+        <td style="font-size: 18px; font-weight: bold; color: #000; padding-bottom: 10px;">Payhub: Daily Report</td>
+        <td style="font-size: 18px; text-align: right; color: #666; padding-bottom: 10px;">${formattedDate}</td>
+      </tr>
+    </table>
+
+    <!-- Total Volume -->
+    <table width="100%" style="margin-bottom: 20px;">
+      <tr>
+        <td style="font-size: 14px;font-weight: bold; color: #444; padding-bottom: 8px;">TOTAL VOLUME</td>
+        <td style="font-size: 14px; font-weight: bold; text-align: right;">${payhub_data}</td>
+      </tr>
+    </table>
+    <!-- Merchants Section -->
+    <h3 style="color: #444; text-transform: uppercase; font-weight: bold; border-bottom: 1px solid #ddd; padding-bottom: 8px; margin-top: 30px;">Merchants</h3>
+    <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+    ${merchant_data
+      .map(
+        (item) => {
+          if (item.yesterday > 0) {
+            return `
+              <tr>
+                <td style="padding: 8px; text-align: left; font-weight: bold;">${(item.business_name || "NA").toUpperCase()}</td>
+                <td style="padding: 8px; text-align: right;">${item.yesterday || 0}</td>
+              </tr>
+            `;
+          }
+          return ''; // If yesterday <= 0, return an empty string, which won't create a row
+        }
+      )
+      .join('')
+    }
+</table>
+
+    <!-- Gateway Section -->
+    <h3 style="color: #444; text-transform: uppercase; font-weight: bold; border-bottom: 1px solid #ddd; padding-bottom: 8px; margin-top: 30px;">Gateway</h3>
+
+
+<table style="width: 100%; border-collapse: collapse;">
+    ${
+      gateway_data.map((item) => {
+        if (item.yesterday > 0) {
+          return `
+            <tr>
+              <td style="padding: 8px; text-align: left; font-weight: bold;">${(item.gatewayName || "NA").toUpperCase()}</td>
+              <td style="padding: 8px; text-align: right;">${item.yesterday || 0}</td>
+            </tr>
+          `;
+        }
+        return ''; // Return empty string if yesterday is 0 or less
+      }).join('')
+    }
+</table>
+          <div style="text-align: center; margin: 15px 0;">
+           <a href="${url}" style="background-color: #007BFF; color: white; padding: 8px 15px; text-decoration: none; border-radius: 4px; font-size: 14px;">Download Full Report</a>
+      </div>
+  </div>
+</div> 
+`
+
+
+    //     html: `
+    //     <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; color: #333;">
+    //   <div style="max-width: 480px; margin: auto; background-color: #fff; padding: 15px; border-radius: 6px; box-shadow: 0 3px 6px rgba(0,0,0,0.1);">
+    //     <h2 style="text-align: center; color: #000; border-bottom: 1px solid #ddd; padding-bottom: 8px;">Payhub: Daily Report</h2>
+        
+    //     <!-- Payhub Section -->
+    //     <h4 style="color: #007BFF; margin: 15px 0 10px; text-align: center;">Payhub Summary</h4>
+    //     <div style="width: 80%; margin: 0 auto;">
+    //       <table style="width: 100%; border-collapse: collapse; font-size: 14px; margin: auto;">
+    //         <tr>
+    //           <td style="padding: 5px; text-align: left;">Total Volume:</td>
+    //           <td style="padding: 5px; text-align: right;"><strong>${payhub_data}</strong></td>
+    //         </tr>
+    //       </table>
+    //     </div>
+
+    //     <!-- Merchants Section -->
+    //     <h4 style="color: #007BFF; margin: 15px 0 10px; text-align: center;">Merchants</h4>
+    //     <div style="width: 80%; margin: 0 auto;">
+    //       <table style="width: 100%; border-collapse: collapse; font-size: 14px; margin: auto;">
+    //         <thead>
+    //           <tr>
+    //             <th style="text-align: left; border-bottom: 1px solid #ddd; padding: 5px;">Name</th>
+    //             <th style="text-align: right; border-bottom: 1px solid #ddd; padding: 5px;">24-Hour Total</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           ${merchant_data
+    //             .map(
+    //               (item) => `
+    //             <tr>
+    //               <td style="padding: 5px;">${item.business_name || "NA"}</td>
+    //               <td style="padding: 5px; text-align: right;">${item.yesterday || 0}</td>
+    //             </tr>
+    //           `
+    //             )
+    //             .join("")}
+    //         </tbody>
+    //       </table>
+    //     </div>
+
+    //     <!-- Gateway Section -->
+    //     <h4 style="color: #007BFF; margin: 15px 0 10px; text-align: center;">Gateways</h4>
+    //     <div style="width: 80%; margin: 0 auto;">
+    //       <table style="width: 100%; border-collapse: collapse; font-size: 14px; margin: auto;">
+    //         <thead>
+    //           <tr>
+    //             <th style="text-align: left; border-bottom: 1px solid #ddd; padding: 5px;">Name</th>
+    //             <th style="text-align: right; border-bottom: 1px solid #ddd; padding: 5px;">24-Hour Total</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           ${gateway_data
+    //             .map(
+    //               (item) => `
+    //             <tr>
+    //               <td style="padding: 5px;">${item.gatewayName || "NA"}</td>
+    //               <td style="padding: 5px; text-align: right;">${item.yesterday || 0}</td>
+    //             </tr>
+    //           `
+    //             )
+    //             .join("")}
+    //         </tbody>
+    //       </table>
+    //     </div>
+
+    //     <div style="text-align: center; margin: 15px 0;">
+    //       <a href="${url}" style="background-color: #007BFF; color: white; padding: 8px 15px; text-decoration: none; border-radius: 4px; font-size: 14px;">Download Full Report</a>
+    //     </div>
+
+    //     <p style="font-size: 12px; color: #666; text-align: center;">For questions, contact us at <a href="mailto:support@payhub.com" style="color: #007BFF; text-decoration: none;">support@payhub.com</a></p>
+    //   </div>
+    // </div>
+        
+    //     `
       };
+
+
+      /* <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; color: #333;">
+  <div style="max-width: 600px; margin: auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);">
+   <!-- Header with Date -->
+    <table width="100%" style="margin-bottom: 20px;">
+      <tr>
+        <td style="font-size: 18px; color: #000; padding-bottom: 10px;">Payhub: Daily Report</td>
+        <td style="font-size: 18px; text-align: right; color: #666; padding-bottom: 10px;">02/11/2024</td>
+      </tr>
+    </table>
+
+    <!-- Total Volume -->
+    <table width="100%" style="margin-bottom: 20px;">
+      <tr>
+        <td style="font-size: 20px; color: #444; padding-bottom: 8px;">TOTAL VOLUME</td>
+        <td style="font-size: 24px; font-weight: bold; text-align: right;">15022513</td>
+      </tr>
+    </table>
+    <!-- Merchants Section -->
+    <h3 style="color: #444; text-transform: uppercase; font-weight: bold; border-bottom: 1px solid #ddd; padding-bottom: 8px; margin-top: 30px;">Merchants</h3>
+    <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+      <tr>
+        <td style="padding: 8px; text-align: left; font-weight: bold;">Gennpay</td>
+        <td style="padding: 8px; text-align: right;">100000</td>
+      </tr>
+      <tr>
+        <td style="padding: 8px; text-align: left; font-weight: bold;">Growwe</td>
+        <td style="padding: 8px; text-align: right;">5022513</td>
+      </tr>
+    </table>
+
+    <!-- Gateway Section -->
+    <h3 style="color: #444; text-transform: uppercase; font-weight: bold; border-bottom: 1px solid #ddd; padding-bottom: 8px; margin-top: 30px;">Gateway</h3>
+    <table style="width: 100%; border-collapse: collapse;">
+      <tr>
+        <td style="padding: 8px; text-align: left; font-weight: bold;">AIRPAY</td>
+        <td style="padding: 8px; text-align: right;">1500000</td>
+      </tr>
+    </table>
+  </div>
+</div> */
+
+
+    //   <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; color: #333;">
+    //   <div style="max-width: 600px; margin: auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+    //     <h2 style="text-align: center; color: #000; border-bottom: 2px solid #000; padding-bottom: 10px;">PayHub</h2>
+    //     <p style="font-size: 16px; line-height: 1.5;">Hello,</p>
+    //     <p style="font-size: 16px; line-height: 1.5;">Here is the 24-hour report of Payhub:</p>
+        
+    //     <!-- Payhub Section -->
+    //     <h3 style="color: #007BFF; border-bottom: 1px solid #ddd; padding-bottom: 10px;">Payhub Total Volume</h3>
+    //     <table style="width: 100%; border-collapse: collapse; margin: 20px 0;">
+    //       <thead>
+    //         <tr>
+    //           <th style="text-align: left; border-bottom: 2px solid #007BFF; padding: 8px;">Name</th>
+    //           <th style="text-align: right; border-bottom: 2px solid #007BFF; padding: 8px;">Total 24 Hour</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         <tr>
+    //           <td style="padding: 8px;">Payhub</td>
+    //           <td style="padding: 8px; text-align: right;">${payhub_data}</td>
+    //         </tr>
+    //       </tbody>
+    //     </table>
+
+    //     <!-- Merchants Section -->
+    //     <h3 style="color: #007BFF; border-bottom: 1px solid #ddd; padding-bottom: 10px;">Merchants</h3>
+    //     <table style="width: 100%; border-collapse: collapse; margin: 20px 0;">
+    //       <thead>
+    //         <tr>
+    //           <th style="text-align: left; border-bottom: 2px solid #007BFF; padding: 8px;">Name</th>
+    //           <th style="text-align: right; border-bottom: 2px solid #007BFF; padding: 8px;">Total 24 Hour</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         ${merchant_data
+    //           .map(
+    //             (item) => `
+    //           <tr>
+    //             <td style="padding: 8px;">${item.business_name || "NA"}</td>
+    //             <td style="padding: 8px; text-align: right;">${item.yesterday || 0}</td>
+    //           </tr>
+    //         `
+    //           )
+    //           .join("")}
+    //       </tbody>
+    //     </table>
+
+    //     <!-- Gateway Section -->
+    //     <h3 style="color: #007BFF; border-bottom: 1px solid #ddd; padding-bottom: 10px;">Gateway</h3>
+    //     <table style="width: 100%; border-collapse: collapse; margin: 20px 0;">
+    //       <thead>
+    //         <tr>
+    //           <th style="text-align: left; border-bottom: 2px solid #007BFF; padding: 8px;">Name</th>
+    //           <th style="text-align: right; border-bottom: 2px solid #007BFF; padding: 8px;">Total 24 Hour</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         ${gateway_data
+    //           .map(
+    //             (item) => `
+    //           <tr>
+    //             <td style="padding: 8px;">${item.gatewayName || "NA"}</td>
+    //             <td style="padding: 8px; text-align: right;">${item.yesterday || 0}</td>
+    //           </tr>
+    //         `
+    //           )
+    //           .join("")}
+    //       </tbody>
+    //     </table>
+
+    //     <p style="font-size: 16px; line-height: 1.5;">You can also download the CSV report by clicking the button below:</p>
+    //     <div style="text-align: center; margin: 20px 0;">
+    //       <a href="${url}" style="background-color: #007BFF; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 18px;">Download Report</a>
+    //     </div>
+    //     <p style="font-size: 16px; line-height: 1.5;">Thank you,</p>
+    //     <p style="font-size: 16px; line-height: 1.5;">The Payhub Team</p>
+    //     <hr style="border: 0; height: 1px; background: #ddd; margin: 20px 0;">
+    //     <small style="color: #666; font-size: 12px;">If you have any questions, feel free to reach out to us at <a href="mailto:support@payhub.com" style="color: #000; text-decoration: none;">support@payhub.com</a></small>
+    //   </div>
+    // </div>
   
       await transporter.sendMail(mailOptions);
       console.log('Report email sent successfully');
