5915fffb76fe4f243191919884b17d1029d71a2c
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-12-09 15:27:27 +0530

    updates

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const { updatePendingTransactionStatus, updatePendingTransactionStatus2, updateP
 //const { connectRedis } = require("./lib/routesAndServices/utils/redis");
 const { findParticularTx } = require("./lib/routesAndServices/utils/transactionDao");
 //const { payhubBankScrapper } = require("./lib/gateways/payhub")
+const client = require('prom-client');
 
 //connectRedis()
 
@@ -33,6 +34,8 @@ const { findParticularTx } = require("./lib/routesAndServices/utils/transactionD
 // updateGatewayVolumeData()
 //payhubBankScrapper()
 //findParticularTx()
+
+
 config.dbConfig((err) => {
   if (err) {
     // logger.error(err, 'exiting the app.');
@@ -47,6 +50,30 @@ config.dbConfig((err) => {
 
   // init express app
   const app = express();
+  const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+// Create custom metrics
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+});
+
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(client.register.metrics());
+});
+
   app.set("trust proxy", true);
 
   // config express
